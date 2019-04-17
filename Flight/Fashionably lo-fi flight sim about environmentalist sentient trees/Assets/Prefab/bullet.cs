cf980816650efa0d80fbf59b0a075b24f269ba64
SeijiTakara <ayasenri@gmail.com>
2019-04-17 03:51:23 -0400

    Flight
    Add

---
+++ Flight/Fashionably lo-fi flight sim about environmentalist sentient trees/Assets/Prefab/bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+    
+{
+    public GameObject Enemy;
+    
+    public float power;
+
+   
+
+    void onTriggerEnter(Collider collider)
+    {
+        Enemy.GetComponent<Rigidbody>().AddForce(0,0,power,ForceMode.Impulse);
+    }
+}
